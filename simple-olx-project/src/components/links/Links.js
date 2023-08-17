345e1de0b69b8daef95d1d3ea3d14142b6dd74bf
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-17 16:35:39 +0500

    lLink component js and css

---
+++ simple-olx-project/src/components/links/Links.js
@@ -0,0 +1,9 @@
+import {HiMiniArrowLongRight} from "react-icons/hi2"
+import "./Links.css"
+export default function Links(){
+    return(
+        <div className="link-container">
+            <a href="#" className="Links">View Products <HiMiniArrowLongRight></HiMiniArrowLongRight></a>
+        </div>
+    )
+}
