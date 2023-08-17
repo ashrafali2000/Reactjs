4c2a23e78642f5c16f6186427581b3c4b488e6ad
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-17 16:35:11 +0500

    input component js and css

---
+++ simple-olx-project/src/components/input/Input.js
@@ -0,0 +1,8 @@
+import "./Input.css"
+export default function Input() {
+    return (
+        <div>
+            <input type="text" placeholder="Search your products here..." className="Inputfield" autoFocus></input>
+        </div>
+    )
+}
