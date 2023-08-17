04aa0846035baeb02e95b50fa68fe043808704b2
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-17 16:31:14 +0500

    categories component js and css

---
+++ simple-olx-project/src/components/categories/Categories.js
@@ -0,0 +1,8 @@
+import "./Categories.css"
+export default function Categories() {
+    return (
+        <div className="categories">
+           <a href="#" className="link">Our Community Sections</a>
+        </div>
+    )
+}
